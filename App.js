85d460c63e7bccfc21db3dd27b77c6fb0e5f155d
rob3748 <114134109+rob3748@users.noreply.github.com>
2025-06-21 23:23:42 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+
+import React from "react";
+import firebaseConfig from "./firebaseConfig";
+console.log("Firebase config loaded", firebaseConfig);
+
+export default function App() {
+  return <div>Hello Encrypted Chat App</div>;
+}
