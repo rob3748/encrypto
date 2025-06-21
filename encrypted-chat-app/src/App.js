e8365b097d4853d9740189557a34faf147a6b366
rob3748 <114134109+rob3748@users.noreply.github.com>
2025-06-21 23:23:06 +0100

    Delete encrypted-chat-app directory

--- encrypted-chat-app/src/App.js
+++
@@ -1,8 +0,0 @@
-
-import React from "react";
-import firebaseConfig from "./firebaseConfig";
-console.log("Firebase config loaded", firebaseConfig);
-
-export default function App() {
-  return <div>Hello Encrypted Chat App</div>;
-}
